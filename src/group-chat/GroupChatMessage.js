120de798171d743a7aaa9c55c75a865018887ce5
David Vílchez <dvilchez@xuaps.com>
2016-06-02 18:50:48 +0200

    Add elapsed time to message object

--- src/group-chat/GroupChatMessage.js
+++ src/group-chat/GroupChatMessage.js
@@ -1,4 +1,8 @@
-const GroupChatMessage = {}
+const GroupChatMessage = {
+    elapsedMinutes(){
+        return Math.round((((Date.now()-this.startingTime) % 86400000) % 3600000) / 60000)
+    }
+}
 
 export default function(dataObjectChild, isMe, identity){
     return Object.assign(Object.create(GroupChatMessage),{
