8a8bd51855ca71d6477fbbdd54d5591430194f6c
luistduarte <lduarte.suil@gmail.com>
2016-06-21 14:39:53 +0100

    some updates to bracelet

--- examples/bracelet/BraceletSensorObserver.js
+++ examples/bracelet/BraceletSensorObserver.js
@@ -1,8 +1,4 @@
 
-
-// "use strict";
-
-
 function hypertyLoaded(result) {
 
   let hypertyObserver;
@@ -18,5 +14,20 @@ function hypertyLoaded(result) {
   let hi = '<p>Hyperty Observer URL: ' + result.runtimeHypertyURL + '</p>';
 
   console.log('Observer Waiting!!');
+  let button = $('.search-btn');
+  button.removeClass('hide');
+
+  let email = $('.email-input');
+  let observer = result.instance;
+  button.on('click', function(event) {
+    observer.discovery(email.val()).then(function(result) {
+      console.log('resultt', result[0]);
+      let collection = $('.collection');
+      let collectionItem = '<li data-url="' + result[0].userID + '" class="collection-item avatar">' +
+      '<span class="title"><b>UserURL: </b>' + result[0].userID + '</span><p>&nbsp;</p>' +
+      '<p>' + result[0].descriptor + '<br>' + result[0].hypertyID + '</p>' +
+      '<a title="Call to ' + result[0].userID + '" class="waves-effect waves-light btn call-btn secondary-content"><i class="material-icons">call</i></a>' +
+      '</li>';
+    });});
 
 }
--- src/bracelet/BraceletSensorObserver.hy.js
+++ src/bracelet/BraceletSensorObserver.hy.js
@@ -1,5 +1,8 @@
+import IdentityManager from 'service-framework/dist/IdentityManager';
 import {Syncher} from 'service-framework/dist/Syncher';
+import Discovery from 'service-framework/dist/Discovery';
 import {divideURL} from '../utils/utils';
+import Search from './Search';
 
 class BraceletSensorObserver {
 
@@ -9,12 +12,29 @@ class BraceletSensorObserver {
     if (!configuration) throw new Error('The configuration is a needed parameter');
 
     let _this = this;
-
+    let identityManager = new IdentityManager(hypertyURL, configuration.runtimeURL, bus);
     _this._domain = divideURL(hypertyURL).domain;
     _this._objectDescURL = 'hyperty-catalogue://catalogue.' + _this._domain + '/.well-known/dataschemas/Context';
 
     console.log('Init BraceletSensorObserver: ', hypertyURL);
     _this._syncher = new Syncher(hypertyURL, bus, configuration);
+    let discovery = new Discovery(hypertyURL, bus);
+    _this._discovery = discovery;
+    console.log('asd1');
+    _this.identityManager = identityManager;
+    console.log('asd3');
+    _this.search = new Search(discovery, identityManager);
+    console.log('asd4');
+  }
+
+  discovery(email)
+  {
+    let _this = this;
+    return new Promise(function(resolve,reject) {
+      _this.search.users([email]).then(function(a) {
+        resolve(a);
+      });
+    });
   }
 
   ObserveBracelet(url) {
