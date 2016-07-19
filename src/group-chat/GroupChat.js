ebfcf53fa02d065e5948940589adb8de09a1fcf5
David Vílchez <dvilchez@xuaps.com>
2016-07-19 09:12:28 +0200

    Refactor GroupChat

--- src/group-chat/GroupChat.js
+++ src/group-chat/GroupChat.js
@@ -1,7 +1,7 @@
 import GroupChatMessage from './GroupChatMessage'
 import GroupChatFile from './GroupChatFile'
 import { Position, Area } from './gps'
-import PeerConnection from './peer-connection'
+import { SendData, addSendDataIfSupported } from './send-data'
 
 const GroupChat = {
 
@@ -36,60 +36,6 @@ const GroupChat = {
     }
 }
 
-const One2OneChat = {
-    onReceiveMessage(data){
-        if (typeof data === 'string') {
-            this.buf = parseInt(data)
-            return
-        }
-
-        this.receiveBuffer.push(data)
-        this.receivedSize += data.byteLength
-
-        if (this.receivedSize === this.buf) {
-            let received = new window.Blob(this.receiveBuffer)
-            this.receiveBuffer = []
-            this.receivedSize = 0
-
-            let url = URL.createObjectURL(received)
-            this.messages.push(GroupChatFile({ file:url, startingTime:Date.now()}, true, {}))
-
-            this._callback(this.messages[this.messages.length-1])
-        }
-    },
-
-    sendFile(file){
-        this.peer.open()
-            .then(connection=>{
-                connection.onData(this.onReceiveMessage.bind(this))
-                connection.sendData(file.size)
-                let chunkSize = 16384
-                let sliceFile = (offset)=> {
-                    let reader = new window.FileReader()
-                    reader.onload = (()=>{
-                        return function(e) {
-                            connection.sendData(e.target.result)
-                            if (file.size > offset + e.target.result.byteLength) {
-                                window.setTimeout(sliceFile, 0, offset + chunkSize)
-                            }
-                        }
-                    })(file)
-                    var slice = file.slice(offset, offset + chunkSize)
-                    reader.readAsArrayBuffer(slice)
-                }
-                sliceFile(0)
-            })
-    },
-
-    onMessage(callback){
-        this.onAddChild((message, identity)=>{
-            this._callback = callback
-            this._processTextMessages(message, identity, callback)
-            this.peerMessage(message)
-        })
-    }
-}
-
 export default function(id, addChild, onAddChild, { name, startingTime, participants }, position, identity){
     let initialData = {
         id: id,
@@ -105,16 +51,8 @@ export default function(id, addChild, onAddChild, { name, startingTime, particip
         receivedSize: 0
     }
 
-    if(participants.length === 2){
-        let peer = PeerConnection({sendMessage: addChild, onMessage: (callback)=>initialData.peerMessage=(msg)=>callback(msg)})
-        let chat = Object.assign(Object.create(GroupChat), One2OneChat, initialData, {peer: peer})
+    let chat = Object.assign(Object.create(GroupChat), initialData)
+	chat = addSendDataIfSupported(chat)
 
-        peer.onConnection((connection)=>{
-            connection.onData(chat.onReceiveMessage.bind(chat))
-        })
-
-        return chat
-    }
-
-    return Object.assign(Object.create(GroupChat), initialData)
+    return chat
 }
