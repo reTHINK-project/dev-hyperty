c986f1925287a3945242037251d472529eb6a502
Steffen Drüsedow <steffen.druesedow@telekom.de>
2016-11-22 15:07:41 +0100

    adapted to updated API of DTWebRTC hyperty

--- examples/dtwebrtc/dtwebrtc.js
+++ examples/dtwebrtc/dtwebrtc.js
@@ -129,25 +129,20 @@ function fillmodal(calleeInfo) {
 // receiving code here
 function initListeners() {
 
-  hyperty.addEventListener('invitation', (identity) => {
-    // preparing the modal dialog with the given identity info
+  hyperty.addEventListener('incomingcall', (identity) => {
+		// preparing the modal dialog with the given identity info
     console.log('incomingcall event received from:', identity);
     $('.invitation-panel').html('<p> Invitation received from:\n ' + identity.email ? identity.email : identity.username + '</p>');
     fillmodal(identity);
     prepareMediaOptions();
-  });
 
-  hyperty.addEventListener('incomingcall', (data) => {
     $('#myModal').find('#btn-accept').on('click', () => {
-      hyperty.invitationAccepted(data);
+      hyperty.acceptCall();
     });
     $('#myModal').find('#btn-reject').on('click', () => {
       hangup();
     });
     $('#myModal').modal('show');
-
-    // if (!confirm('Incoming call. Answer?')) return false;
-    // hyperty.invitationAccepted(data);
   });
 
   hyperty.addEventListener('localvideo', (stream) => {
