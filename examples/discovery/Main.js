adb9de793e742d72144247adaf1e747d0dba141d
David Vílchez <dvilchez@xuaps.com>
2017-03-13 16:06:31 +0100

    Update discovery example

--- examples/discovery/Main.js
+++ examples/discovery/Main.js
@@ -3,23 +3,29 @@ import Logo from './Logo'
 const Main = React.createClass({
 
     onUserListChanged() {
-      console.log('new user event')
-      this.props.hyperty.queryUsers()
-        .then(users => {
-            console.log('users', users)
-            this.setState({users: users})
-        })
+        this.setState({users: this.props.hyperty.queryUsers()})
     },
 
     getInitialState(){
-      console.log('dicoveryH')
       this.props.hyperty.onUserListChanged(this.onUserListChanged)
-      this.props.hyperty.queryUsers()
-        .then(users => {
-            this.setState({users: users})
-        })
 
-      return {users:[]};
+      return {users:this.props.hyperty.queryUsers()};
+    },
+
+    handleInputChange (event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+          [name]: value
+        });
+    },
+
+    search () {
+        let filter = this.state.filter?JSON.parse(this.state.filter):null
+        let result = this.props.hyperty.queryUsers(filter)
+        this.setState({search_result: JSON.stringify(result)})
     },
 
     render() {
@@ -28,6 +34,20 @@ const Main = React.createClass({
                 <div className="row">
                     <div className="col-sm-12">
                         <a href="#">Users <span className="badge">{this.state.users.length}</span></a>
+                    </div>
+                </div>
+                <div className="row">
+                    <form>
+                      <div className="form-group">
+                        <label htmlFor="search">Search</label>
+                        <input type="text" name="filter" onChange={this.handleInputChange} className="form-control" id="search" placeholder="{'username':'openidtest20@gmail.com'}"/>
+                      </div>
+                      <button type="button" className="btn btn-default" onClick={this.search}>Search</button>
+                    </form>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        {this.state.search_result}
                     </div>
                 </div>
         </div>);
