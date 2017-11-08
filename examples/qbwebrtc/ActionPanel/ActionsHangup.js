1268c74e8fa89dfb30c356e4be129743c3cf6843
pchainho <pchainho@gmail.com>
2017-11-08 14:24:21 +0000

    removed outdated hyperties

--- examples/qbwebrtc/ActionPanel/ActionsHangup.js
+++
@@ -1,9 +0,0 @@
-const ActionsHangup = ({hangup}) => {
-  return (<div className="row">
-            <form className="form-inline">
-                <a href="#" onClick={hangup} className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">call_end</i></a>
-            </form>
-        </div>);
-}
-
-export default ActionsHangup
