5974ad011c26e7cba4b6bbcdbc5db4420c1b264c
RAFAEL SALGUEIRO FELGUEIRAS PINTO <P058076@PTPORTUGAL.CORPPT.COM>
2018-04-10 14:53:55 +0100

    updated

--- src/wallet-dsm/WalletDSM.hy.js
+++ src/wallet-dsm/WalletDSM.hy.js
@@ -2,7 +2,7 @@ import { Syncher } from 'service-framework/dist/Syncher';
 import URI from 'urijs';
 import Search from '../utils/Search';
 import IdentityManager from 'service-framework/dist/IdentityManager';
-import {Discovery} from 'service-framework/dist/Discovery';
+import { Discovery } from 'service-framework/dist/Discovery';
 
 class WalletDSM {
 
@@ -24,43 +24,73 @@ class WalletDSM {
   }
 
 
-  start(){
+  start() {
     let _this = this;
 
+    // send request to create wallet (to subscription manager in Vertx P2P Stub)
+    /*
+    _this.search.myIdentity().then(identity => {
+      _this.identity = identity;
+      console.log('WalletDSM identity', identity);
+    });
+    */
+
+    // TODO - userUrl
     let userURL = 'user://sharing-cities-dsm/userWalletIdentity';
+
+
     let createMessage = {
       type: 'forward', to: 'hyperty://sharing-cities-dsm/wallet-manager', from: _this.hypertyURL,
-      identity: { userProfile : { userURL: userURL }},
+      identity: { userProfile: { userURL: userURL } },
       body: {
         from: _this.hypertyURL,
         to: 'hyperty://sharing-cities-dsm/wallet-manager',
         type: 'create'
-      }};
+      }
+    };
+
 
     console.log('WalletDSM create message', createMessage);
 
-    _this.bus.postMessage(createMessage,  (reply) => {
+    _this.bus.postMessage(createMessage, (reply) => {
 
 
       console.log('WalletDSM create Reply', reply);
+      
       if (reply.body.code == 200) {
+        
+        debugger;
+        // wallet was created -> subscribe
         _this.myWallet = reply.body.wallet;
         let subscriptionURL = reply.body.wallet.address + '/subscription';
 
         let subscribeMessage = {
           type: 'forward', to: 'hyperty://sharing-cities-dsm/wallet-manager', from: _this.hypertyURL,
-          identity: { userProfile : { userURL: userURL }},
+          identity: { userProfile: { userURL: userURL } },
           body: {
             from: _this.hypertyURL,
             to: subscriptionURL,
             type: 'create'
           },
-          address: reply.body.wallet.address};
-          _this.bus.postMessage(subscribeMessage,  (reply2) => {
-            console.log('WalletDSM subscription Reply', reply2);
-           });
+          address: reply.body.wallet.address
+        };
+        _this.bus.postMessage(subscribeMessage, (reply2) => {
+          console.log('WalletDSM subscription Reply', reply2);
+
+          if (reply2.body.code == 200) {
+            // TODO - listen for updates
+            _this.bus.addListener(_this.hypertyURL + '/changes', (msg) => {
+              _this.update(msg);
+            });
+          }
+        });
       }
     });
+  }
+
+  update(msg) {
+    console.log('WalletDSM update', msg);
+
   }
 }
 export default function activate(hypertyURL, bus, config) {
