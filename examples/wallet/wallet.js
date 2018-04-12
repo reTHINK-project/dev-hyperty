079081b4d36ddfa0dd487d0a7b47df38f4dac06b
suil <lduarte.suil@gmail.com>
2018-04-12 11:36:10 +0100

    update hyperty wallet

--- examples/wallet/wallet.js
+++ examples/wallet/wallet.js
@@ -1,5 +1,18 @@
 
 function hypertyLoaded(result) {
-    console.log("hyperty loaded", result.instance);
+    console.log("Wallet hyperty loaded", result.instance);
 
+    result.instance.identityManager.discoverUserRegistered().then(function(identity) {
+      hypertyReady(result, identity);
+    });
+}
+
+function hypertyReady(result, identity) {
+  console.log("Wallet hyperty Ready", result, identity);
+
+  function afterUpdate(event){
+      console.log('WAllet new update', event);
+      $('.token-value').text(event);
+  }
+  result.instance.start(afterUpdate, identity);
 }
--- src/wallet/Wallet.hy.js
+++ src/wallet/Wallet.hy.js
@@ -16,103 +16,50 @@ class Wallet {
     this.currentPosition;
     this.bus = bus;
     this.hypertyURL = hypertyURL;
-    this.myWallet = null;
-    console.log(this.identityManager);
-
-    //debugger;
-    // this.start();
-
     bus.addListener(hypertyURL, (msg) => {
-      console.log('Wallet new msg', msg);
+      console.log('[Wallet] new msg', msg);
     });
   }
 
-
-  start(callback) {
+  start(callback, identity) {
     let _this = this;
-
-
-    // send request to create wallet (to subscription manager in Vertx P2P Stub)
-
-
-    /*
-        _this.identityManager.discoverUserRegistered().then(result => {
-          console.log('asd',result);
-          debugger;
-        }).catch(result => {
-          console.log('asd',result);
-          debugger;
-        });*/
-
-
-    // TODO - userUrl
-    let userURL = 'user://google.com/rafaelfelgueiras1993@gmail.com';
-
 
     let createMessage = {
       type: 'forward', to: 'hyperty://sharing-cities-dsm/wallet-manager', from: _this.hypertyURL,
-      identity: { userProfile: { userURL: userURL } },
+      identity: { userProfile: { userURL: identity.userURL } },
       body: {
-        from: _this.hypertyURL,
-        to: 'hyperty://sharing-cities-dsm/wallet-manager',
-        type: 'create'
+        type: 'create',
+        from: _this.hypertyURL
       }
     };
-
-
-    console.log('Wallet create message', createMessage);
+    /*Create Message should be like THIS
+    *
+    *  let createMessage = {
+    *    type: 'create', to: 'hyperty://sharing-cities-dsm/wallet-manager', from: _this.hypertyURL,
+    *    identity: { userProfile: { userURL: userURL } },
+    *  };
+    *
+    */
+    console.log('[Wallet] create message', createMessage);
 
     _this.bus.postMessage(createMessage, (reply) => {
 
-
-      console.log('Wallet create Reply', reply);
+      console.log('[Wallet] create Reply', reply);
       if (reply.body.code == 200) {
-
         _this.syncher.subscribe(_this.objectDescURL, reply.body.reporter_url, true, false, true, null).then(function(obj) {
           console.log('[Wallet] subscribe result :', obj);
 
           callback(obj.data.balance);
 
           obj.onChange('*', (event) => {
-            console.log('[VertxAppProtoStub] New Change :', event);
-
+            console.log('[Wallet] New Change :', event);
             callback(event.data);
-
-
           });
 
         }).catch(function(error) {
-
-          console.log('[VertxAppProtoStub] error', error);
+          console.log('[Wallet] error', error);
         });
 
-
-        /*
-
-                // wallet was created -> subscribe
-                _this.myWallet = reply.body.wallet;
-                let subscriptionURL = reply.body.wallet.address + '/subscription';
-
-                let subscribeMessage = {
-                  type: 'forward', to: 'hyperty://sharing-cities-dsm/wallet-manager', from: _this.hypertyURL,
-                  identity: { userProfile: { userURL: userURL } },
-                  body: {
-                    from: _this.hypertyURL,
-                    to: subscriptionURL,
-                    type: 'create'
-                  },
-                  address: reply.body.wallet.address
-                };
-                _this.bus.postMessage(subscribeMessage, (reply2) => {
-                  console.log('Wallet subscription Reply', reply2);
-
-                  if (reply2.body.code == 200) {
-                    // TODO - listen for updates
-                    _this.bus.addListener(_this.hypertyURL + '/changes', (msg) => {
-                      _this.update(msg);
-                    });
-                  }
-                });*/
       }
     });
   }
