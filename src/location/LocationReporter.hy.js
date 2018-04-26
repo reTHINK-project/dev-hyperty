480445d50d6bd598a2dca33a1eea224848e15d98
suil <lduarte.suil@gmail.com>
2018-04-26 17:28:04 +0100

    resuming dor on location reporter

--- src/location/LocationReporter.hy.js
+++ src/location/LocationReporter.hy.js
@@ -22,6 +22,63 @@ class LocationHypertyFactory {
     this.hypertyURL = hypertyURL;
     this.reporter = null;
     this.watchID = null;
+
+
+  }
+
+
+
+  _getRegisteredUser() {
+    let _this = this;
+
+    return new Promise((resolve, reject) => {
+      _this.identityManager.discoverUserRegistered().then((identity) => {
+        console.log('[LocationReporter] GET MY IDENTITY:', identity);
+        resolve(identity);
+      }).catch((error) => {
+        console.error('[LocationReporter] ERROR:', error);
+        reject(error);
+      });
+    });
+
+  }
+
+  _resumeReporters() {
+    let _this = this;
+    //debugger;
+    return new Promise((resolve, reject) => {
+      _this.syncher.resumeReporters({store: true}).then((reporters) => {
+        console.log('[LocationReporter] Reporters resumed', reporters);
+
+        let reportersList = Object.keys(reporters);
+
+        if (reportersList.length  > 0) {
+
+          _this._getRegisteredUser().then((identity) => {
+
+            reportersList.forEach((dataObjectReporterURL) => {
+              //debugger;
+              console.log(identity);
+              _this.identity = identity;
+              console.log('[LocationReporter] ', dataObjectReporterURL);
+              console.log('[LocationReporter]', reporters[dataObjectReporterURL]);
+
+              if (identity.userURL == reporters[dataObjectReporterURL].metadata.subscriberUsers[0] && reporters[dataObjectReporterURL].metadata.name == 'location') {
+                //debugger;
+                _this.reporter = reporters[dataObjectReporterURL];
+                return resolve(true);
+              } else {
+                return resolve(false);
+              }
+            });
+          });
+        } else {
+          return resolve(false);
+        }
+      }).catch((reason) => {
+        console.info('[LocationReporter] Reporters:', reason);
+      });
+    });
   }
 
   //FOR invite checkin  -> vertx://sharing-cities-dsm/token-rating-checkin
@@ -86,27 +143,31 @@ class LocationHypertyFactory {
   }
 
   initPosition() {
+    //debugger;
     let _this = this;
-    _this.syncher.create(_this.objectDescURL, [], position(), true, false, 'location', {}, { resources: ['location-context'] })
-      .then((reporter) => {
-        _this.reporter = reporter;
-        console.log('[LocationReporter]  DataObjectReporter', _this.reporter);
-        reporter.onSubscription((event) => event.accept());
-        _this.search.myIdentity().then(identity => {
-          _this.identity = identity;
-          navigator.geolocation.watchPosition((position) => {
-            console.log('[LocationReporter] my position: ', position);
-            _this.currentPosition = position;
-
-            // reporter.data.values = [
-            //   { name: 'latitude', unit: 'lat', value: position.coords.latitude},
-            //   { name: 'longitude', unit: 'lon', value: position.coords.longitude }
-            // ];
-            // reporter.data.time = position.timestamp;
-            // reporter.data.tag = identity.preferred_username;
+    if (_this.reporter == null) {
+      _this.syncher.create(_this.objectDescURL, [], position(), true, false, 'location', {}, { resources: ['location-context'] })
+        .then((reporter) => {
+          _this.reporter = reporter;
+          console.log('[LocationReporter]  DataObjectReporter', _this.reporter);
+          reporter.onSubscription((event) => event.accept());
+          _this.search.myIdentity().then(identity => {
+            _this.identity = identity;
+            _this.setCurrentPosition();
           });
-        });
       });
+    } else {
+      _this.setCurrentPosition();
+    }
+}
+
+  setCurrentPosition() {
+    let _this = this;
+    navigator.geolocation.watchPosition((position) => {
+      console.log('[LocationReporter] my position: ', position);
+      _this.currentPosition = position;
+      //debugger;
+    });
   }
 
   updateLocation() {
