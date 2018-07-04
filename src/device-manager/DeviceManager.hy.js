2c7e4c4799de39f2780933ed64b18702d8939964
suil <lduarte.suil@gmail.com>
2018-07-04 11:07:09 +0100

    create stream using platform and userID

--- src/device-manager/DeviceManager.hy.js
+++ src/device-manager/DeviceManager.hy.js
@@ -26,6 +26,7 @@ class DeviceManager {
     let _this = this;
     _this.identity = identity;
 
+
     let createMessage = {
       type: 'forward', to: 'runtime://sharing-cities-dsm/protostub/smart-iot', from: _this.hypertyURL,
       identity: { userProfile: { userURL: _this.identity.userURL, guid: _this.identity.guid } },
@@ -45,7 +46,8 @@ class DeviceManager {
     });
   }
 
-  createStream() {
+  //'edp', 'luisuserID'
+  createStream(platformID, platformUID) {
     let _this = this;
 
 
@@ -57,8 +59,8 @@ class DeviceManager {
         from: _this.hypertyURL,
         resource: 'stream',
         platformID: 'edp',
-        platformUID: 'luisuserID',
-        ratingType: 'private'
+        platformUID: platformID,
+        ratingType: platformUID
       }
     };
 
@@ -71,75 +73,12 @@ class DeviceManager {
 
   /*    if (reply.body.code == 200) {
 
-        let objURL = 'context://sharing-cities-dsm/' + createMessage.body.platformID + '/' + createMessage.body.platformUID;
-
-        _this._resumeObservers(objURL).then(function(result) {
-
-          //  debugger;
-
-          if (result != false) {
-            console.log('[DeviceManager] Resume result :', result);
-
-
-            result.onChange('*', (event) => {
-              console.log('[DeviceManager] New Change :', event);
-            });
-
-          } else {
-            _this.syncher.subscribe(_this.objectDescURL, objURL, true, false, true, null).then(function(obj) {
-              console.log('[DeviceManager] subscribe result :', obj);
-
-
-              obj.onChange('*', (event) => {
-                console.log('[Wallet] New Change :', event);
-                callback(event);
-              });
-
-            }).catch(function(error) {
-              console.log('[Wallet] error', error);
-            });
-          }
-        }).catch(function(error) {
-
-        });
-
-
       }*/
 
 
     });
   }
 
-  _resumeObservers(deviceManagerURL) {
-    let _this = this;
-
-    return new Promise((resolve, reject) => {
-      //debugger;
-      _this.syncher.resumeObservers({store: true}).then((observers) => {
-
-
-        console.log('[DeviceManager] Resuming observer : ', observers, _this);
-
-        let observersList = Object.keys(observers);
-        if (observersList.length  > 0) {
-          //debugger;
-          observersList.forEach((dataObjectObserverURL) => {
-            console.log('[DeviceManager].syncher.resumeObserver: ', dataObjectObserverURL);
-            if (deviceManagerURL == dataObjectObserverURL) {
-              return resolve(observers[dataObjectObserverURL]);
-            }
-          });
-        } else {
-          resolve(false);
-        }
-        resolve(false);
-
-      }).catch((reason) => {
-        console.info('[DeviceManager] Resume Observer | ', reason);
-
-      });
-    });
-  }
 
 }
 export default function activate(hypertyURL, bus, config) {
