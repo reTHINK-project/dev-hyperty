a8a4d52fb47d7ea48c98e96f9f4b823d6a72155f
RAFAEL SALGUEIRO FELGUEIRAS PINTO <P058076@PTPORTUGAL.CORPPT.COM>
2018-07-09 12:27:47 +0100

    updated wallet example for public wallets support

--- examples/wallet/wallet.js
+++ examples/wallet/wallet.js
@@ -14,6 +14,7 @@ function hypertyLoaded(result, runtimeLoader = null) {
 }
 
 function hypertyReady(result, identity) {
+  /*
   console.log('Wallet hyperty Ready', result, identity);
 
   function afterUpdate(event) {
@@ -26,5 +27,25 @@ function hypertyReady(result, identity) {
 
   const profileInfo = { ageRange: '18-25', workplace: 'Lisbon', cause: '0' };
   identity.profile = profileInfo;
+  result.instance.start(afterUpdate, identity);
+  */
+
+  // get public wallets
+  console.log('Wallet hyperty Ready', result, identity);
+
+  function afterUpdate(event) {
+    console.log('WAllet new update', event);
+    if (event.field == 'balance') {
+      $('.token-value').text(event.data);
+
+    }
+  }
+
+  identity = {
+    profile: {
+      guid: 'user-guid://public-wallets'
+    }
+  };
+
   result.instance.start(afterUpdate, identity);
 }
