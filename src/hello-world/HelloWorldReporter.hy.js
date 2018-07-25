25eb849ec8268a1caf32d421e80559602a692442
Paulo Chainho <pchainho@gmail.com>
2018-07-25 10:50:41 +0100

    Hello world reporter: updated to use the Syncher created by the SandboxFactory

--- src/hello-world/HelloWorldReporter.hy.js
+++ src/hello-world/HelloWorldReporter.hy.js
@@ -1,6 +1,6 @@
 /* jshint undef: true */
 
-import {Syncher} from 'service-framework/dist/Syncher';
+//import {Syncher} from 'service-framework/dist/Syncher';
 import {divideURL} from '../utils/utils';
 import hello from './hello';
 
@@ -15,11 +15,12 @@ class HelloWorldReporter {
   * Create a new HelloWorldReporter
   * @param  {Syncher} syncher - Syncher provided from the runtime core
   */
-  constructor(hypertyURL, bus, configuration) {
+  constructor(hypertyURL, bus, configuration, factory) {
 
     if (!hypertyURL) throw new Error('The hypertyURL is a needed parameter');
     if (!bus) throw new Error('The MiniBus is a needed parameter');
     if (!configuration) throw new Error('The configuration is a needed parameter');
+    if (!factory) throw new Error('The factory is a needed parameter');
 
 
     let _this = this;
@@ -27,8 +28,10 @@ class HelloWorldReporter {
     let domain = divideURL(hypertyURL).domain;
     _this._domain = domain;
     _this._objectDescURL = 'hyperty-catalogue://catalogue.' + domain + '/.well-known/dataschema/HelloWorldDataSchema';
+    _this._factory = factory;
+    debugger;
 
-    let syncher = new Syncher(hypertyURL, bus, configuration);
+    let syncher = _this._factory.createSyncher(hypertyURL, bus, configuration);
 
     _this._syncher = syncher;
 
@@ -127,11 +130,11 @@ class HelloWorldReporter {
 
 
 
-export default function activate(hypertyURL, bus, configuration) {
+export default function activate(hypertyURL, bus, configuration, factory) {
 
   return {
     name: 'HelloWorldReporter',
-    instance: new HelloWorldReporter(hypertyURL, bus, configuration)
+    instance: new HelloWorldReporter(hypertyURL, bus, configuration, factory)
   };
 
 }
