89e3a49f4d0765c5fe7a7ff8320ce40ecab34379
RAFAEL SALGUEIRO FELGUEIRAS PINTO <rafaelfelgueiras1993@gmail.com>
2018-08-04 22:32:25 +0100

    Wallet + example: ranking updates

--- examples/wallet/wallet.js
+++ examples/wallet/wallet.js
@@ -6,7 +6,7 @@ function hypertyLoaded(result, runtimeLoader = null) {
     runtimeLoader.requireProtostub('sharing-cities-dsm');
   }
 
-  result.instance.identityManager.discoverUserRegistered().then(function(identity) {
+  result.instance.identityManager.discoverUserRegistered().then(function (identity) {
     hypertyReady(result, identity);
   });
 
@@ -18,10 +18,13 @@ function hypertyReady(result, identity) {
   console.log('Wallet hyperty Ready', result, identity);
 
   function afterUpdate(event) {
-    console.log('WAllet new update', event);
+    console.log('Wallet new update', event);
     if (event.field == 'balance') {
       $('.token-value').text(event.data);
 
+    }
+    else if (event.field == 'ranking') {
+      $('.wallet-ranking').text(event.data);
     }
   }
 
