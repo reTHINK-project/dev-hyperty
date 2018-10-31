ccabb0b628e12fdf07a34ffb699d05d347816062
Paulo Chainho <pchainho@gmail.com>
2018-10-31 14:25:29 +0000

    eLearningDemo updated to invite eLearning backend

--- examples/learning/learningPlayerDemo.js
+++ examples/learning/learningPlayerDemo.js
@@ -13,21 +13,29 @@ function hypertyLoaded(result) {
 
     console.info('[LearningPlayerDemo] quizzes: ', result);
 
+    elearningPlayer._resumeReporters().then((result) => {
 
+      console.log('[LearningPlayerDemo] resume ', result);
+
+      if (result.length === 0 ) {
+        elearningPlayer.invite('hyperty://sharing-cities-dsm/elearning').then(() => {
+          console.log('[LearningPlayerDemo] invited "hyperty://sharing-cities-dsm/elearning"');
+        });
+      }
+
+
+
+      elearningPlayer.initQuizSubmission();
+  
+    }).catch((reason) => {
+      console.info('[LearningPlayerDemo] start failed | ', reason);
+    });
+
+  
   }).catch((reason) => {
     console.info('[LearningPlayerDemo] start failed | ', reason);
   });
 
-  elearningPlayer._resumeReporters().then((result) => {
-
-    elearningPlayer.initQuizSubmission();
-
-    
-    
-
-  }).catch((reason) => {
-    console.info('[LearningPlayerDemo] start failed | ', reason);
-  });
 }
 
 
