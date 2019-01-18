9d09638d49b7991b7ec7f5ef58a85fb1ad3e2b79
Paulo Chainho <pchainho@gmail.com>
2019-01-18 13:20:02 +0000

    UserActivityObserver example fix

--- examples/user-activity/UserActivityObserver.js
+++ examples/user-activity/UserActivityObserver.js
@@ -25,93 +25,96 @@ function hypertyLoaded(result, runtimeLoader = null) {
     runtimeLoader.requireProtostub('sharing-cities-dsm');
     // load stub according to OS
     // if (isIOS) {
-    runtimeLoader.requireProtostub('fitness.strava.com');
-    domain = 'strava.com';
-    // }
-    // else {
-    //   runtimeLoader.requireProtostub('fitness.google.com');
-    // idp = 'google.com';
-    // }
-
-
-    runtimeLoader.authorise(domain, 'user_activity_context').then(function (value) {
-    }).catch(function (err) {
-      console.log(err);
-
+    runtimeLoader.requireProtostub('fitness.strava.com').then(()=>{
+      domain = 'strava.com';
+      // }
+      // else {
+      //   runtimeLoader.requireProtostub('fitness.google.com');
+      // idp = 'google.com';
+      // }
+  
+  
+      runtimeLoader.authorise(domain, 'user_activity_context').then(function (value) {
+        const walletName = 'Wallet';
+        import('../../dev-hyperty/dist/' + walletName + '.hy')
+          .then((hypertyModule) => {
+            runtimeLoader.requireHyperty(walletUrl).then(function (res) {
+              // your code
+              console.log('loaded wallet ', res);
+              const wallet = res;
+    
+              function getDistance(activities) {
+                let distance = 0;
+                activities.forEach(function (element) {
+                  distance += element.data.distance;
+                });
+                return distance;
+              }
+    
+              function getTokens(activities) {
+                let distance = 0;
+                activities.forEach(function (element) {
+                  distance += element.value;
+                });
+                return distance;
+              }
+    
+    
+              // store in window
+              window.wallet = wallet;
+    
+              function afterUpdate(event) {
+    
+                if (event.field === 'transactions') {
+                  console.log('wallet event: ', event.data);
+                  const activities = event.data.filter(trans => trans.source === 'user_activity');
+    
+                  // walking
+                  const walkingActivities = activities.filter(activ => activ.data.activity === 'user_walking_context');
+    
+                  // distance for activity
+                  const walkingDistance = getDistance(walkingActivities);
+                  $('.walking-distance').text(walkingDistance);
+    
+                  // tokens for activity
+                  const walkingTokens = getTokens(walkingActivities);
+                  $('.walking-tokens').text(walkingTokens);
+    
+                  // biking
+                  const bikingActivities = activities.filter(activ => activ.data.activity === 'user_biking_context');
+    
+    
+                  // TODO - parse and show info on sessions
+                  $('.token-amount').text(event.data.balance);
+                }
+              }
+    
+              res.identityManager
+                .discoverUserRegistered()
+                .then(function (identity) {
+                  let profileInfo = {};
+                  identity.profile = profileInfo;
+                  wallet.start(afterUpdate, identity);
+                });
+            })
+              .catch(function (reason) {
+                console.error(reason);
+              });
+          });        
+        result.identityManager.discoverUserRegistered().then(function (identity) {
+          hypertyReady(result, identity);
+        });
+          }).catch(function (err) {
+        console.log(err);
+  
+      });
+  
     });
 
-    const walletName = 'Wallet';
-    import('../../dev-hyperty/dist/' + walletName + '.hy')
-      .then((hypertyModule) => {
-        runtimeLoader.requireHyperty(walletUrl).then(function (res) {
-          // your code
-          console.log('loaded wallet ', res);
-          const wallet = res;
 
-          function getDistance(activities) {
-            let distance = 0;
-            activities.forEach(function (element) {
-              distance += element.data.distance;
-            });
-            return distance;
-          }
-
-          function getTokens(activities) {
-            let distance = 0;
-            activities.forEach(function (element) {
-              distance += element.value;
-            });
-            return distance;
-          }
-
-
-          // store in window
-          window.wallet = wallet;
-
-          function afterUpdate(event) {
-
-            if (event.field === 'transactions') {
-              console.log('wallet event: ', event.data);
-              const activities = event.data.filter(trans => trans.source === 'user_activity');
-
-              // walking
-              const walkingActivities = activities.filter(activ => activ.data.activity === 'user_walking_context');
-
-              // distance for activity
-              const walkingDistance = getDistance(walkingActivities);
-              $('.walking-distance').text(walkingDistance);
-
-              // tokens for activity
-              const walkingTokens = getTokens(walkingActivities);
-              $('.walking-tokens').text(walkingTokens);
-
-              // biking
-              const bikingActivities = activities.filter(activ => activ.data.activity === 'user_biking_context');
-
-
-              // TODO - parse and show info on sessions
-              $('.token-amount').text(event.data.balance);
-            }
-          }
-
-          res.identityManager
-            .discoverUserRegistered()
-            .then(function (identity) {
-              let profileInfo = {};
-              identity.profile = profileInfo;
-              wallet.start(afterUpdate, identity);
-            });
-        })
-          .catch(function (reason) {
-            console.error(reason);
-          });
-      });
 
   }
 
-  result.identityManager.discoverUserRegistered().then(function (identity) {
-    hypertyReady(result, identity);
-  });
 
 
 }
